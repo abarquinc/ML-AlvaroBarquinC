305171a6b7fb87052f2cd93914915fa447fbe33d
alvarobarquinc <alvarobarquinc@gmail.com>
2023-11-14 16:13:04 -0300

    Retocando detalles

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001;
 app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}`));
 
 
-app.get('/', (req, res) => {
+app.get('/home.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
 
