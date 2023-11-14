e715c59404a7e6a5b0f7a8c8135bf5a328bc4e69
alvarobarquinc <alvarobarquinc@gmail.com>
2023-11-14 15:46:34 -0300

    Correcion de los links de registro y login

--- src/app.js
+++ src/app.js
@@ -14,3 +14,12 @@ app.listen (port, () => console.log(`Servidor funcionando bien en puerto ${port}
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 })
+
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+})
